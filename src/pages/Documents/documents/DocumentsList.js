d23ba488d62640dc744c8c7aeaf540ad21c100ce
Bartosz Stolc <bartst010395@gmail.com>
2019-08-07 21:55:56 +0200

    Memoization with useCallback

--- src/pages/Documents/documents/DocumentsList.js
+++ src/pages/Documents/documents/DocumentsList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { DocsList, Warning } from './DocumentsList.styles';
 import {
@@ -12,19 +12,18 @@ const DocumentsList = () => {
   const dispatch = useDocumentsDispatch();
   const { documents, loading } = useDocumentsState();
 
-  useEffect(() => {
-    fetchDocuments();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const fetchDocuments = async () => {
+  const fetchDocuments = useCallback(async () => {
     try {
       const { data } = await axios.get('/documents');
       dispatch({ type: 'GET_DOCUMENTS', payload: data });
     } catch (err) {
       console.log(err.response.data);
     }
-  };
+  }, [dispatch]);
+
+  useEffect(() => {
+    fetchDocuments();
+  }, [fetchDocuments]);
 
   if (!documents || loading) return <SpinnerSm />;
 
